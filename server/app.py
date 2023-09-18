10f13a5c29243a444f390960c65c95e151d60446
Tony Hunter <thunter1987@yahoo.com>
2023-09-18 00:02:41 -0500

    Logout class passes

--- server/app.py
+++ server/app.py
@@ -76,6 +76,18 @@ class Login(Resource):
                 return user.to_dict(), 200
 
         return {'error': '401 Unauthorized'}, 401
+    
+    class Logout(Resource):
+    
+    def delete(self):
+        
+        if session.get('user_id'):
+            
+            session['user_id'] = None
+            
+            return {}, 204
+        
+        return {'error': '401 Unauthorized'}, 401
 
 class RecipeIndex(Resource):
     pass
