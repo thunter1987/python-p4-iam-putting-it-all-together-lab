747f6a567b86242693a6343bd084c435540e31b4
Tony Hunter <thunter1987@yahoo.com>
2023-09-18 00:08:18 -0500

    all tests passed

--- server/app.py
+++ server/app.py
@@ -100,6 +100,37 @@ class RecipeIndex(Resource):
             return [recipe.to_dict() for recipe in user.recipes], 200
         
         return {'error': '401 Unauthorized'}, 401
+    
+    def post(self):
+
+        if session.get('user_id'):
+
+            request_json = request.get_json()
+
+            title = request_json['title']
+            instructions = request_json['instructions']
+            minutes_to_complete = request_json['minutes_to_complete']
+
+            try:
+
+                recipe = Recipe(
+                    title=title,
+                    instructions=instructions,
+                    minutes_to_complete=minutes_to_complete,
+                    user_id=session['user_id'],
+                )
+
+                db.session.add(recipe)
+                db.session.commit()
+
+                return recipe.to_dict(), 201
+
+            except IntegrityError:
+
+                return {'error': '422 Unprocessable Entity'}, 422
+
+        return {'error': '401 Unauthorized'}, 401
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
