15f012a0a2d24938557a064d98af3fa832c300f4
Tony Hunter <thunter1987@yahoo.com>
2023-09-17 19:15:35 -0500

    models.Recipe updated

--- server/models.py
+++ server/models.py
@@ -35,5 +35,16 @@ class User(db.Model, SerializerMixin):
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
-    
+    __table_args__ = (
+        db.CheckConstraint('length(instructions) >= 50'),
+    )
+
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f'<Recipe {self.id}: {self.title}>'
